2a2cd0e214cb372a0dbb7a75fa0b5d5e7dbababb
Manish19Kumar <manishkumar28007@gmail.com>
2019-01-01 23:54:45 +0530

    first commit

---
+++ Sources/App/Models/Acronym.swift
@@ -0,0 +1,28 @@
+
+import Vapor
+import FluentSQLite
+final class Acronym: Codable {
+    var id: Int?
+    var short: String
+    var long: String
+    init(short: String, long: String) {
+        self.short = short
+        self.long = long
+    }
+}
+
+//extension Acronym: Model {
+//    // 1. Tell Fluent what database to use for this model. The template is already configured to use SQLite.
+//    typealias Database = SQLiteDatabase
+//    // 2. Tell Fluent what type the ID is.
+//    typealias ID = Int
+//    // 3. Tell Fluent the key path of the model’s ID property.
+//    public static var idKey: IDKey = \Acronym.id
+//}
+extension Acronym: SQLiteModel {
+    
+}
+
+extension Acronym: Migration {}
+
+extension Acronym: Content {}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -11,10 +11,14 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
-
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+    
+    // 1. Register a new route at /api/acronyms that accepts a POST request and returns Future<Acronym>. It returns the acronym once it’s saved.
+    router.post("api", "acronyms") { req -> Future<Acronym> in
+        // 2. Decode the request’s JSON into an Acronym model using Codable. This returns a Future<Acronym> so it uses a flatMap(to:) to extract the acronym when the decoding is complete. Note this is different from how data is decoded in Chapter 2, “Hello Vapor!”. In this route handler, you are calling decode(_:) on Request yourself. You are then unwrapping the result as decode(_:) returns a Future<Acronym>.
+        return try req.content.decode(Acronym.self)
+            .flatMap(to: Acronym.self) { acronym in
+                // 3. Save the model using Fluent. This returns Future<Acronym> as it returns the model once it’s saved.
+                return acronym.save(on: req)
+        }
+    }
 }
